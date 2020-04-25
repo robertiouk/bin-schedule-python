bb0981797c705606e6ea2e822d22465ce4eae37c
robertiouk <robertiouk@gmail.com>
2020-04-25 20:46:24 +0100

    fixed url

--- bin_schedule_api.py
+++ bin_schedule_api.py
@@ -2,8 +2,8 @@ import requests
 from datetime import datetime
 from functools import reduce
 
-ADDRESS_BY_POSTCODE_URL = 'https://bbcpro-jeapi.azurewebsites.net/api/addresssearch/bypostcode?Postcode=mk43' \
-                          '%208ht&Language=ENG '
+ADDRESS_BY_POSTCODE_URL = 'https://bbcpro-jeapi.azurewebsites.net/api/addresssearch/bypostcode?Postcode=ab12' \
+                          '%23cd&Language=ENG '
 COLLECTION_BY_UPRN = 'https://bbaz-as-prod-bartecapi.azurewebsites.net/api/bincollections/residential/getbyuprn' \
                      '/10002965475 '
 
